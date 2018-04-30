5ad1a287f417e013b1fba98f7a6f5487deb24f97
akapparova <akapparova@stuy.edu>
2018-04-30 14:19:06 -0400

    fixed

--- parser.py
+++ parser.py
@@ -8,24 +8,19 @@ The file follows the following format:
      Every command is a single character that takes up a line
      Any command that requires arguments must have those arguments in the second line.
      The commands are as follows:
-
          push: push a copy of the current top of the coordinate system stack to the stack
-
          pop: pop off the current top of the coordinate system stack
-
          All the shape commands work as follows:
              1) Add the shape to a temporary matrix
              2) Multiply that matrix by the current top of the coordinate system stack
              3) Draw the shape to the screen
              4) Clear the temporary matrix
-
          sphere: add a sphere -
                  takes 4 arguemnts (cx, cy, cz, r)
          torus: add a torus -
                 takes 5 arguemnts (cx, cy, cz, r1, r2)
          box: add a rectangular prism -
               takes 6 arguemnts (x, y, z, width, height, depth)
-
          circle: add a circle -
                  takes 3 arguments (cx, cy, r)
          hermite: add a hermite curve -
@@ -34,20 +29,16 @@ The file follows the following format:
                  takes 8 arguments (x0, y0, x1, y1, x2, y2, x3, y3)
          line: add a line -
                takes 6 arguemnts (x0, y0, z0, x1, y1, z1)
-
          All the transformation commands work as follows:
              1) Create the appropriate transformation matrix
              2) Multiply that matrix by current top of the coordinate system stack
-
          scale: takees 3 arguments (sx, sy, sz)
          move:  takes 3 arguments (tx, ty, tz)
          rotate: takes 2 arguments (axis, theta) axis should be x, y or z
-
          display: display the screen
          save: save the screen to a file -
                takes 1 argument (file name)
          quit: end parsing
-
 See the file script for an example of the file format
 """
 ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
@@ -131,7 +122,7 @@ def parse_file( fname, edges, polygons, transform, screen, zbuffer, color ):
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), float(args[5]) )
             matrix_mult( systems[-1], edges )
-            draw_lines(eges, screen, zbuffer, color)
+            draw_lines(edges, screen, zbuffer, color)
             edges = []
 
         elif line == 'scale':
